1fa57de56d5d6a75373e1a277e6283c416606951
womacks <samuel.womack@wwt.com>
2015-11-18 10:22:43 -0600

    created dependency injection for utilizing the spark api within the webapp, wrote post and put request to those services

--- pocsparks.js
+++ pocsparks.js
@@ -1,16 +1,21 @@
 var express = require('express'),
     bodyParser = require('body-parser');
 
+var sparkAdminToken = require('./config').token;
+
+// DI
+var sparkServFactory = require('./lib/sparkService'),
+    sparkService = sparkServFactory(sparkAdminToken),
+    sparkCntrlFactory = require('./lib/sparkController'),
+    sparkController = sparkCntrlFactory(sparkService);
+
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 
-app.post('/sn-test', function(req, res) {
-  console.log(req.body);
-
-  res.status(201).send(req.body);
-});
+app.post('/sn-test', sparkController.pocStarter);
+app.put('/sn-test/:id', sparkController.pocUpdate);
 
 app.listen(5000, function() {
   console.log('Server Listening on Port 5000');
