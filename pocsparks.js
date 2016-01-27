2097305a4971e8b1ba656e51e273bdef0baa94d8
drkchiloll <samwomack_@me.com>
2016-01-27 15:06:18 -0600

    remove merge conflicts

--- pocsparks.js
+++ pocsparks.js
@@ -8,12 +8,6 @@ var express = require('express'),
 var sparkAdminToken = require('./config').token;
 
 // DI
-<<<<<<< HEAD
-var sparkServFactory = require('./lib/sparkService'),
-    sparkService = sparkServFactory(sparkAdminToken),
-    sparkCntrlFactory = require('./lib/sparkController'),
-    sparkController = sparkCntrlFactory(sparkService);
-=======
 var sparkServDev = require('./lib/sparkServiceDev'),
     sparkServiceDev = sparkServDev(sparkAdminToken),
     sparkServProd = require('./lib/sparkServiceProd'),
@@ -22,7 +16,6 @@ var sparkServDev = require('./lib/sparkServiceDev'),
     sparkControllerDev = sparkCntrlDev(sparkServiceDev),
     sparkCntrlProd = require('./lib/sparkControllerProd'),
     sparkControllerProd = sparkCntrlProd(sparkServiceProd);
->>>>>>> c714fbec91e6e56af63f51991b841b00ee69f943
 
 var app = express();
 
@@ -31,14 +24,7 @@ app
   .use(bodyParser.urlencoded({extended : false}))
   .use(express.static('./public'));
 
-<<<<<<< HEAD
-app.post('/sn-test', sparkController.pocStarter);
-app.put('/sn-test/:id', sparkController.pocUpdate);
-app.get('/sn-test/:atc', sparkController.queryPoc);
-app.delete('/sn-test/:atc', sparkController.popMember);
-app.delete('/rmrecord/:atc', sparkController.removeRecord);
-=======
-app.post('/snspark', sparkControllerProd.pocStart);
+app.post('/snspark', sparkControllerProd.pocStarter);
 app.put('/snspark', sparkControllerProd.pocUpdate);
 
 app.get('/snspark/:atc', sparkControllerProd.queryPoc);
@@ -52,19 +38,14 @@ app.put('/sn-test/:id', sparkControllerDev.pocUpdate);
 app.get('/sn-test/:atc', sparkControllerDev.queryPoc);
 app.delete('/sn-test/:atc', sparkControllerDev.popMember);
 app.delete('/rmrecordDev/:atc', sparkControllerDev.removeRecord);
->>>>>>> c714fbec91e6e56af63f51991b841b00ee69f943
 
 app.get('/fileupload/:filename', function(req, res) {
   var fileName = req.params.filename;
   res.sendFile(__dirname+'/public/files/' + fileName);
 });
 
-<<<<<<< HEAD
-app.post('/pochooks', sparkController.handleHooks);
-=======
 app.post('/pochooksProd', sparkControllerProd.handleHooks);
 app.post('/pochooksDev', sparkControllerDev.handleHooks);
->>>>>>> c714fbec91e6e56af63f51991b841b00ee69f943
 
 app.listen(5000, function() {
   console.log('Server Listening on Port 5000');
